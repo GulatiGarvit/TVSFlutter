ce33890dc4a246221331f9ba68ea5ef1ba1323d8
GulatiGarvit <ggulati_be22@thapar.edu>
2025-11-26 13:56:52 +0530

    try video feed fix

--- lib/video_feed.dart
+++ lib/video_feed.dart
@@ -1,14 +1,10 @@
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'data_service.dart';
 
-enum StreamType { camera, dehazed }
-
-class VideoFeed extends StatelessWidget {
+class VideoFeed extends StatefulWidget {
   final String title;
   final DataService dataService;
   final StreamType streamType;
-
+  
   const VideoFeed({
     super.key,
     required this.title,
@@ -17,31 +13,55 @@ class VideoFeed extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
-      alignment: Alignment.center,
-      child: StreamBuilder<Uint8List>(
-        stream:
-            streamType == StreamType.camera
-                ? dataService.cameraStream
-                : dataService.dehazedStream,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Text(
-              "Waiting for $title...",
-              style: const TextStyle(fontSize: 18, color: Colors.white70),
-            );
-          }
+  State<VideoFeed> createState() => _VideoFeedState();
+}
 
-          return Image.memory(
-            snapshot.data!,
-            gaplessPlayback: true,
-            filterQuality: FilterQuality.low,
-            fit: BoxFit.contain,
+class _VideoFeedState extends State<VideoFeed> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<ui.Image?>(
+      stream: widget.streamType == StreamType.camera
+          ? widget.dataService.cameraFrameStream
+          : widget.dataService.dehazedFrameStream,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData || snapshot.data == null) {
+          return Container(
+            color: Colors.black,
+            child: Center(
+              child: Text(
+                'Waiting for ${widget.title}...',
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
           );
-        },
-      ),
+        }
+
+        return SizedBox.expand(
+          child: FittedBox(
+            fit: BoxFit.cover, // CHANGED FROM BoxFit.contain TO BoxFit.cover
+            child: SizedBox(
+              width: snapshot.data!.width.toDouble(),
+              height: snapshot.data!.height.toDouble(),
+              child: CustomPaint(
+                painter: ImagePainter(snapshot.data!),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
+}
+
+class ImagePainter extends CustomPainter {
+  final ui.Image image;
+  ImagePainter(this.image);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawImage(image, Offset.zero, Paint());
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
